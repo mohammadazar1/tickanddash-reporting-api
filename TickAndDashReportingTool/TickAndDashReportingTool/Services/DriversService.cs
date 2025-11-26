1ff0696015d555234719ad410b0020460dc24c9a
mohammadazar1 <105586426+mohammadazar1@users.noreply.github.com>
2025-11-26 13:46:31 +0200

    Update DriversService.cs

--- TickAndDashReportingTool/TickAndDashReportingTool/Services/DriversService.cs
+++ TickAndDashReportingTool/TickAndDashReportingTool/Services/DriversService.cs
@@ -5,8 +5,8 @@ using TickAndDashDAL.Models;
 using TickAndDashReportingTool.Controllers.V1.Requests;
 using TickAndDashReportingTool.Helpers;
 using TickAndDashReportingTool.HttpClients.DigitalCodex.Interfaces;
-using TickAndDashReportingTool.HttpClients.DigitalCodex.Requests;
-using TickAndDashReportingTool.HttpClients.DigitalCodex;
+using TickAndDashReportingTool.HttpClients.DigitalCodex.DTOs;
+
 using TickAndDashReportingTool.Services.Interfaces;
 
 namespace TickAndDashReportingTool.Services
