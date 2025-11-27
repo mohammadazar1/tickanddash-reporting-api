97164ea54824fe5b33a74df70d69350b05417000
mohammadazar1 <105586426+mohammadazar1@users.noreply.github.com>
2025-11-27 11:12:40 +0200

    Update Program.cs

--- TickAndDashReportingTool/TickAndDashReportingTool/Program.cs
+++ TickAndDashReportingTool/TickAndDashReportingTool/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Serilog;
-using Serilog.Events;
 using System;
 
 namespace TickAndDashReportingTool
@@ -10,33 +8,19 @@ namespace TickAndDashReportingTool
     {
         public static void Main(string[] args)
         {
-            // Configure Serilog logging
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .WriteTo.Console() // For local debugging
-                .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day) // For Azure/Kudu logs
-                .Enrich.FromLogContext()
-                .CreateLogger();
-
             try
             {
-                Log.Information("Starting TickAndDash Reporting Tool API...");
                 CreateHostBuilder(args).Build().Run();
             }
             catch (Exception ex)
             {
-                Log.Fatal(ex, "API crashed on startup!");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
+                Console.WriteLine("Unhandled exception: " + ex);
+                throw;
             }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog() // 🔥 Activating Serilog
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
